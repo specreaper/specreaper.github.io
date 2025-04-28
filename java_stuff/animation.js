03905bde8e903d857068ae8e3c7367daa3e6f350
Sean Evans <sean.evans1.2020@gmail.com>
2025-04-28 12:33:28 -0400

    animation.js

--- java_stuff/animation.js
+++ java_stuff/animation.js
@@ -2,53 +2,92 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 let x = 200;
+let dx = (Math.random() * 5);
 let y = 200;
-let dx = 2;
-let dy = 3;
-const moveSpeed = 3;
+let dy = (Math.random() * 5); 
+
+const player = {
+    x : 60,
+    y : 60,
+    color: 'green',
+    speed: 3
+};
+
+const keys = {};
 
 function drawRect(x,y) {
-    console.log("drawing rect");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(x,y,50,50);
     ctx.fill();
 }
-function 
-function updateObjectPosition() {
-            if (keys['w']) {
-                objectY -= moveSpeed;
-            }
-            if (keys['s']) {
-                objectY += moveSpeed;
-            }
-            if (keys['a']) {
-                objectX -= moveSpeed;
-            }
-            if (keys['d']) {
-                objectX += moveSpeed;
-            }
-}
-function animate() {
-    	x += dx;
-	y += dy; 
-	drawRect(x,y);
 
-	if(x>=canvas.width-50){
-		dx *= -1;
-	}
-	if (y>=canvas.height-50){
-                dy *= -1;
-        }
-	if (x<=0) {
-		dx *= -1;
-	}
-	if (y<=0) {
-		dy *= -1;
-	}
+function drawPlayer(){
+    ctx.fillStyle = player.color;
+    ctx.beginPath();
+    ctx.arc(
+        player.x, //accessing the variable called x inside the player object
+        player.y,
+        20,
+        0,
+        2*Math.PI
+    );
+    ctx.fill();
+}
+
+function movePlayer(){
+    if(keys['s']){
+        player.y += player.speed;
+    }
+    if(keys['w']){
+        player.y -= player.speed;
+    }
+    if(keys['a']){
+        player.x -= player.speed;
+    }
+    if(keys['d']){
+        player.x += player.speed;
+    }
+}
+
+function moveEnemy(){
+    x = x + dx;
+    y = y + dy;
+
+    if(x > 400){
+        dx = dx * -1;
+    }
+    if(x < 0){
+        dx = dx * -1;
+    }
+
+    if(y > 400){
+        dy = dy * -1;
+    }
+    if(y < 0){
+        dy = dy * -1;
+    }
+}
+
+function animate() {
+    drawRect(x,y);
+    movePlayer();
+    drawPlayer();
+    moveEnemy();
 
     requestAnimationFrame(animate);
 }
 
-//call our function
+function handleKeyPress(e){
+    keys[e.key] = true;
+}
+
+
+document.addEventListener('keydown', handleKeyPress);
+
+document.addEventListener('keyup', (e) => {
+    keys[e.key] = false;
+});
+
 animate();
+
